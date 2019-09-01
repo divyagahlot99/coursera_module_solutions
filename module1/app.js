476f7dd3c12a7333f3f6c550db6fc430d47e1287
DivyaGahlot <gahlotdivya1019@gmail.com>
2019-09-01 22:55:25 +0530

    Assignment 1 submission

--- module1/app.js
+++ module1/app.js
@@ -1,1 +1,32 @@
+(function() {
+    'use strict';
 
+    angular.module('LunchCheck', [])
+        .controller('LCController', LCController);
+    LCController.$inject = ['$scope'];
+
+    function LCController($scope) {
+        $scope.meals = '';
+        $scope.display = '';
+        $scope.checked = false;
+
+        $scope.checkLunch = function() {
+            if ($scope.meals.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var arraymeals = $scope.meals.split(',');
+                var arraymealsWithoutEmptys = arraymeals.filter(function(v) {
+                    return v.trim() !== '';
+                });
+
+                if (arraymealsWithoutEmptys.length <= 3) {
+                    $scope.display = 'Enjoy!';
+                } else {
+                    $scope.display = 'Too much!';
+                }
+            }
+        };
+    }
+})();
